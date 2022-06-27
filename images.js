8b1b9b8c99f3e8912646a1dc1e0e815496215095
KALKIDAN MISGANAW <38901615+kalkida@users.noreply.github.com>
2022-06-27 16:29:44 +0300

    Add files via upload

---
+++ images.js
@@ -0,0 +1,20 @@
+const wallieLogo = require("../assets/images/wallie-logo.png");
+const banner = require("../assets/images/banner.png");
+const promoBanner = require("../assets/images/promo-banner.png");
+const focus = require("../assets/images/focus.png");
+const Tele = require("../assets/images/tele.png")
+
+// Dummy
+const usFlag = require("../assets/images/us-flag.jpg");
+
+
+export default {
+    wallieLogo,
+    banner,
+    promoBanner,
+    focus,
+    Tele,
+
+    // Dummy
+    usFlag
+}
